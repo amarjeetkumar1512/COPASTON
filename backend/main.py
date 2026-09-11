0f924ba441d43fc0675cc96ce21dc8a5d27c4792
amarjeetkumar1512 <eramarjeetkumar95@gmail.com>
2026-09-11 22:53:21 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -28,6 +28,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173"
+        "https://copaston-3011k24y8-copaston.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
